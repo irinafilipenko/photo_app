6cb22c22fd2625f928042c687ba6bd2aed34eee9
irinafilipenko <abyoo15532@gmail.com>
2024-07-21 07:33:16 +0200

    add folder service and refactor repository

--- lib/repositories/photo_repository.dart
+++ lib/repositories/photo_repository.dart
@@ -1,16 +1,13 @@
+import 'package:photo_app/components/constants.dart';
 import 'package:photo_app/models/photo_model.dart';
 import 'package:dio/dio.dart';
+import 'package:photo_app/service/photo_service.dart';
 
 class PhotoRepository {
-  final Dio _dio = Dio();
+  final PhotoService _photoService = PhotoService();
 
   Future<List<PhotoModel>> fetchBeers() async {
-    final response = await _dio.get(
-        'https://api.pexels.com/v1/curated?per_page=50',
-        options: Options(headers: {
-          'Authorization':
-              'dwzhuZSMsaIXZQHz9YV6Pziu9G78uLYbAoc2pSvrwF8H7yR6IcDm56FP'
-        }));
+    final response = await _photoService.getPhotos(kUrlPhoto, kApiKeyPhoto);
     return (response.data['photos'] as List)
         .map((json) => PhotoModel.fromJson(json))
         .toList();
--- lib/repositories/user_repository.dart
+++ lib/repositories/user_repository.dart
@@ -1,11 +1,13 @@
 import 'package:dio/dio.dart';
+import 'package:photo_app/components/constants.dart';
 import 'package:photo_app/models/user_model.dart';
+import 'package:photo_app/service/user_service.dart';
 
 class UserRepository {
-  final Dio _dio = Dio();
+  final UserService _userService = UserService();
 
   Future<UserModel> login(String email, String password) async {
-    final response = await _dio.get('https://randomuser.me/api/');
+    final response = await _userService.getUser(kUrlUser);
     return UserModel.fromJson(response.data['results'][0]);
   }
 }
